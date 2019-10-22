d76cfb20e8ce536c7e2e36ed6bdf4aa3c35504f6
Paplex <pierre-adrien.plessix@mines-paristech.fr>
2019-10-22 23:07:00 +0200

    Newton Ordre 2

--- Proj naif.py
+++ Proj naif.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import autograd as autograd
-import autograd.numpy as np
+import numpy as np
 
 
-g= lambda X : (X[1])**2 +(X[0])**2
+g= lambda X : (X[1])**2 -(X[0])**2
 g1=lambda X : np.exp(-X[0]**2-X[1]**2)
 g2=lambda X : np.exp( -(X[0]-1)**2-(X[1]-1)**2)
 
@@ -12,7 +12,7 @@ def f(X):
 
 
 def find_seed_D (g,c=0,limx=[0.,1.],limy=[0.,1.],eps=2**(-26)):
-    if (g([limx[1],limy[0]])-c)*(g([limx[0],limy[0]])-c)<=0:
+    if (g([limx[1],limy[0]])-c)*(g([limx[0],limy[0]])-c)<0:
         a,b=limx[0],limx[1]
         if g([a,limy[0]])>g([b,limy[0]]):
             a,b=b,a
@@ -39,6 +39,8 @@ def find_seed_L (g,c=0,limx=[0.,1.],limy=[0.,1.],eps=2**(-26)):
         return [limx[0],d]
     else :
         return None
+
+
 def find_seed_U (g,c=0,limx=[0.,1.],limy=[0.,1.],eps=2**(-26)):
     if (g([limx[0],limy[1]])-c)*(g([limx[1],limy[1]])-c)<=0 :
         a,b=limx[0],limx[1]
@@ -53,6 +55,7 @@ def find_seed_U (g,c=0,limx=[0.,1.],limy=[0.,1.],eps=2**(-26)):
         return [d,limy[1]]
     else:
         return None
+    
 def find_seed_R(g,c=0,limx=[0.,1.],limy=[0.,1.],eps=2**(-26)):
     if  (g([limx[1],limy[1]])-c)*(g([limx[1],limy[0]])-c)<=0 :
         a,b=limy[0],limy[1]
@@ -67,55 +70,29 @@ def find_seed_R(g,c=0,limx=[0.,1.],limy=[0.,1.],eps=2**(-26)):
         return [limx[1],d]
     else:
         return None
-def cart_pol(position,delta):
-    return np.arccos(position[0]/delta)
-def pol_cart(theta,delta):
-    return np.array([delta*np.cos(theta),delta*np.sin(theta)])
-def simple_contour(g,xc,yc,nom_bord,i,j,c=0,delta=0.01,eps=2**(-26)):
+
+def simple_contour(g,xc,yc,nom_bord,i,j,c=0,delta=0.001):
     abscisses , ordonnées = [],[]
     dic_fonction = {"UP" : find_seed_U, "LEFT":find_seed_L, "RIGHT" : find_seed_R,"DOWN" : find_seed_D }
-    position = dic_fonction[nom_bord](g,c,[xc[i],xc[i+1]], [yc[j],yc[j+1]],2**(-26))
+    position = dic_fonction[nom_bord](g,c,[xc[i],xc[i+1]], [yc[j],yc[j+1]])
     gradg=autograd.grad(g)
-    
-    
-    def h(theta,position):
-                return (f(position+[delta*np.cos(theta),delta*np.sin(theta)])-c)
-    def derh(theta,position1):
-        grad = gradg(position1+np.array([delta*np.cos(theta),delta*np.sin(theta)]))
-        deriv=np.array([(-1)*delta*np.sin(theta),delta*np.cos(theta)])
-        return grad@deriv
-    
-    if  not isinstance(position,list):
+    if  not isinstance( position,list) :
         return [],[]
     else:
-        position = np.array(position) 
-        print("amorce",position) 
+        position = np.array(position)        
         abscisses.append(position[0])
         ordonnées.append(position[1])
+
         def test (position):
             return xc[i]<=position[0]<=xc[i+1] and yc[j]<=position[1]<=yc[j+1]
-
         while test(position) :
             gradX=gradg(position)
             norme = np.sqrt(gradX[1]**2+gradX[0]**2)
             vect = np.array([gradX[1]/norme,-1*gradX[0]/norme])
-            
-            
-            position_faux = position + vect*delta
-            print(position_faux)
-            #Méthode de Newton  
-            position_relat=position_faux-position
-            theta0 = cart_pol(position_relat,delta)
-            theta=theta0-h(theta0,position)/derh(theta0,position)
-            
-            while (theta-theta0)>=eps:
-                theta0 = theta
-                theta = theta0-h(theta0,position)/derh(theta0,position)
-            position = position+pol_cart(theta,delta)
-            print(position)
+            position = position + vect*delta
+
             abscisses.append(position[0])
             ordonnées.append(position[1])
-            print(test(position))
         return abscisses,ordonnées
 
 def contour(f, c=0, xc = [0,1], yc = [0,1], delta = 0.01):
@@ -136,3 +113,6 @@ def trace(g,xc,yc,c=0):
         plt.plot(x,y)
 
 
+for i in [0,1,0.5,1.5,-0.5,-1,-1.5]:
+    trace(f,np.linspace(-3,3,10),np.linspace(-3,3,10),i)
+plt.show()
--- ProjInfo.py
+++ ProjInfo.py
@@ -3,7 +3,7 @@ import autograd as autograd
 import autograd.numpy as np
 
 
-g= lambda X : (X[1])**2 +(X[0])**2
+g= lambda X : (X[1])**2 -(X[0])**2
 g1=lambda X : np.exp(-X[0]**2-X[1]**2)
 g2=lambda X : np.exp( -(X[0]-1)**2-(X[1]-1)**2)
 
@@ -12,7 +12,7 @@ def f(X):
 
 
 def find_seed_D (g,c=0,limx=[0.,1.],limy=[0.,1.],eps=2**(-26)):
-    if (g([limx[1],limy[0]])-c)*(g([limx[0],limy[0]])-c)<=0:
+    if (g([limx[1],limy[0]])-c)*(g([limx[0],limy[0]])-c)<0:
         a,b=limx[0],limx[1]
         if g([a,limy[0]])>g([b,limy[0]]):
             a,b=b,a
@@ -39,6 +39,8 @@ def find_seed_L (g,c=0,limx=[0.,1.],limy=[0.,1.],eps=2**(-26)):
         return [limx[0],d]
     else :
         return None
+
+
 def find_seed_U (g,c=0,limx=[0.,1.],limy=[0.,1.],eps=2**(-26)):
     if (g([limx[0],limy[1]])-c)*(g([limx[1],limy[1]])-c)<=0 :
         a,b=limx[0],limx[1]
@@ -53,6 +55,7 @@ def find_seed_U (g,c=0,limx=[0.,1.],limy=[0.,1.],eps=2**(-26)):
         return [d,limy[1]]
     else:
         return None
+    
 def find_seed_R(g,c=0,limx=[0.,1.],limy=[0.,1.],eps=2**(-26)):
     if  (g([limx[1],limy[1]])-c)*(g([limx[1],limy[0]])-c)<=0 :
         a,b=limy[0],limy[1]
@@ -67,58 +70,66 @@ def find_seed_R(g,c=0,limx=[0.,1.],limy=[0.,1.],eps=2**(-26)):
         return [limx[1],d]
     else:
         return None
-def cart_pol(position,delta):
-    return np.arccos(position[0]/delta)
-def pol_cart(theta,delta):
-    return np.array([delta*np.cos(theta),delta*np.sin(theta)])
-def simple_contour(g,xc,yc,nom_bord,i,j,c=0,delta=0.01,eps=2**(-26)):
+
+
+def simple_contour(g,xc,yc,nom_bord,i,j,c=0,delta=0.001,eps=2**(-26)):
     abscisses , ordonnées = [],[]
     dic_fonction = {"UP" : find_seed_U, "LEFT":find_seed_L, "RIGHT" : find_seed_R,"DOWN" : find_seed_D }
-    position = dic_fonction[nom_bord](g,c,[xc[i],xc[i+1]], [yc[j],yc[j+1]],2**(-26))
+    position = dic_fonction[nom_bord](g,c,[xc[i],xc[i+1]], [yc[j],yc[j+1]])
+    print(position)
     gradg=autograd.grad(g)
-    
-    
-    def h(theta,position):
-                return (f(position+[delta*np.cos(theta),delta*np.sin(theta)])-c)
-    def derh(theta,position1):
-        grad = gradg(position1+np.array([delta*np.cos(theta),delta*np.sin(theta)]))
-        deriv=np.array([(-1)*delta*np.sin(theta),delta*np.cos(theta)])
-        return grad@deriv
-    
-    if  not isinstance(position,list):
+    if  not isinstance( position,list) :
         return [],[]
     else:
-        position = np.array(position) 
-        print("amorce",position) 
+        position = np.array(position)     
         abscisses.append(position[0])
         ordonnées.append(position[1])
+        print(position)
+
         def test (position):
             return xc[i]<=position[0]<=xc[i+1] and yc[j]<=position[1]<=yc[j+1]
-
         while test(position) :
             gradX=gradg(position)
             norme = np.sqrt(gradX[1]**2+gradX[0]**2)
             vect = np.array([gradX[1]/norme,-1*gradX[0]/norme])
-            
-            
             position_faux = position + vect*delta
-            print(position_faux)
-            #Méthode de Newton  
-            position_relat=position_faux-position
-            theta0 = cart_pol(position_relat,delta)
-            theta=theta0-h(theta0,position)/derh(theta0,position)
+            position1=position
+
+            def h1 (x,y):
+                return g(np.array([x,y]))-c
+            def h2 (x,y):
+                return (x - position1[0])**2 + (y - position1[1])**2 - delta**2
+            def H(x,y) : 
+                return np.array([h1(x,y),h2(x,y)])
             
-            while (theta-theta0)>=eps:
-                theta0 = theta
-                theta = theta0-h(theta0,position)/derh(theta0,position)
-            position = position+pol_cart(theta,delta)
-            print(position)
-            abscisses.append(position[0])
-            ordonnées.append(position[1])
-            print(test(position))
+            def J_H(x,y):
+                
+                j = autograd.jacobian
+                return np.c_[j(H, 0)(x,y), j(H, 1)(x,y)]
+            
+            
+            J=J_H(position_faux[0],position_faux[1])
+            if np.linalg.det(J)==0:
+                abscisses.append(position_faux[0])
+                ordonnées.append(position_faux[1])
+            else:
+
+                J_inv=np.linalg.inv(J)
+                position=position_faux -J_inv@H(position_faux[0],position_faux[1])
+                while np.linalg.norm(position-position_faux)>eps:
+                    position_faux=position
+                    J=J_H(position_faux[0],position_faux[1])
+                    if np.linalg.det(J)==0:
+                        break
+                    else:
+                        J_inv=np.linalg.inv(J)
+                        position=position_faux -J_inv@H(position_faux[0],position_faux[1])
+                abscisses.append(position[0])
+                ordonnées.append(position[1])
+
         return abscisses,ordonnées
 
-def contour(f, c=0, xc = [0,1], yc = [0,1], delta = 0.01):
+def contour(f, c=0, xc = [0,1], yc = [0,1], delta = 0.001):
     xs,ys= [], []
 
     liste_bord = ['UP','LEFT','RIGHT','DOWN']
@@ -135,4 +146,6 @@ def trace(g,xc,yc,c=0):
     for x,y in zip(a,b):
         plt.plot(x,y)
 
-
+#for i in [0,1,0.5,1.5,-0.5,-1,-1.5]:
+   # trace(f,np.linspace(-3,3,3),np.linspace(-3,3,3),i)
+#plt.show()
